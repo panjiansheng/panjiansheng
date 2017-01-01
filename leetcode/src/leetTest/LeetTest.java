3934e6903a485edee77e33b8625492151a07fd07
panjiansheng <panjiansheng2008@163.com>
2017-01-01 22:53:44 +0800

    网易

--- leetcode/src/leetTest/LeetTest.java
+++ leetcode/src/leetTest/LeetTest.java
@@ -1,39 +1,96 @@
 package leetTest;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Properties;
+
 public class LeetTest {
 
+	static class A {
+		public A next;
+		public int val;
+
+		public A(int val) {
+			this.val = val;
+		}
+
+		@Override
+		public String toString() {
+			return "A [next=" + next + ", val=" + val + "]";
+		}
+
+	}
+
+	static class C extends A{
+
+		int c=2;
+		int val=3;
+		public C(int val) {
+			super(val);
+			// TODO Auto-generated constructor stub
+		}
+		public String toString() {
+			return String.valueOf(c);
+		}
+		
+	}
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i = 0; i < 10; i++) {
-			System.out.println(123456789 / (int) Math.pow(10, i) % 10);
-		}
-		A a = new A(1);
-		a.next = new A(2);
-		a.next.next = new A(3);
+		int 我=1;
+		System.out.println(我);
+		System.out.println(0f/0f);
 
-		A b = a,d;
-		A c=a.next;
 		
-		c.next=null;
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
+        System.out.println("i j");
+        search:
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 50; j++) {
+                    if (j == 3)
+                        break search;
+                    System.out.println(i+" "+j);
+                }
+            }
+        
+        
+       // FileInputStream fis=new FileInputStream("");
+//        FileDescriptor fd=fis.get
+        System.out.println(System.getProperty("user.dir"));
+        Properties properties=System.getProperties();
+        properties.entrySet();
+        Iterator iterator=properties.entrySet().iterator();
+        while (iterator.hasNext()) {
+			Object object = (Object) iterator.next();
+			System.out.println(object);
+		}
+		Enumeration enumeration=properties.propertyNames();
+		while (enumeration.hasMoreElements()) {
+			Object object = (Object) enumeration.nextElement();
+			System.out.println(object);
+		}
+		
+		A a=new C(1);
+		System.out.println(a.val);
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println(123456789 / (int) Math.pow(10, i) % 10);
+//		}
+//		A a = new A(1);
+//		a.next = new A(2);
+//		a.next.next = new A(3);
+//
+//		A b = a,d;
+//		A c=a.next;
+//		
+//		c.next=null;
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
 	}
 
 }
 
-class A {
-	public A next;
-	public int val;
-
-	public A(int val) {
-		this.val = val;
-	}
-
-	@Override
-	public String toString() {
-		return "A [next=" + next + ", val=" + val + "]";
-	}
-
-}
---
+++ leetcode/src/leetTest/中文测试.java
@@ -0,0 +1,28 @@
+package leetTest;
+
+public class 中文测试  implements C,B{
+	
+	public static String 中文字符串="Java可以用中文变量名";
+	Boolean 中文变量名测试=true;
+	
+	public boolean 测试是否可以使用中文(){
+		return 中文变量名测试?true:false;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		中文测试 测试=new 中文测试();
+		if (测试.测试是否可以使用中文()) {
+			
+			System.out.println(中文字符串);
+		}
+		//System.out.println(测试.val);
+	}
+}
+
+interface C{
+	int val=1;
+}
+interface B{
+	String val="2";
+}
